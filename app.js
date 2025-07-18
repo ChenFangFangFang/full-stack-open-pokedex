5502d82d254c219a2e9cbbef771bc6c14ad6ce25
ChenFangFangFang <sophiachan0430@gmail.com>
2025-07-18 15:12:01 +0300

    edit port number again

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 
 // get the port from env variable based on environment
 const PORT = process.env.NODE_ENV === 'test'
-  ? 5001  // 测试环境使用 5001
+  ? 8080
   : (process.env.PORT || 5000)  // 生产环境使用 PORT 环境变量或 5000
 
 app.use(express.static('dist'))
