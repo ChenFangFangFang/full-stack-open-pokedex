d96c45e47dafd07b8002920b6f306457939d6903
ChenFangFangFang <sophiachan0430@gmail.com>
2025-07-18 14:43:27 +0300

    update port number

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8080',
+    baseUrl: 'http://localhost:5001',
     specPattern: 'cypress/e2e/**/*.cy.js',
     supportFile: 'cypress/support/e2e.js',
   },
