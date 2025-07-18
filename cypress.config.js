61660c569c69cc6ccc8c23d524eebc9ccd2351c0
ChenFangFangFang <sophiachan0430@gmail.com>
2025-07-18 15:39:00 +0300

    11.09 done

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5000',  // 修改为测试环境端口
+    baseUrl: 'http://localhost:5000',
     specPattern: 'cypress/e2e/**/*.cy.js',
     supportFile: 'cypress/support/e2e.js',
   },
