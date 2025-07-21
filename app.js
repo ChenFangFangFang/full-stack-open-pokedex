fba6fe872c79ee15d76c20140b3b31e367b0a3d0
ChenFangFangFang <sophiachan0430@gmail.com>
2025-07-21 15:57:17 +0300

    Update version to 2

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ const PORT = process.env.NODE_ENV === 'test'
   : (process.env.PORT || 5000)
 app.use(express.static('dist'))
 app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+  res.send('2') // change this string to ensure a new version deployed
 })
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw new Error('error...  ')
   res.send('ok')
 })
 app.listen(PORT, () => {
