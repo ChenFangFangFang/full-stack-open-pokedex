fb5bc394f392bfa367f87ebffcf9ae86f0756c01
ChenFangFangFang <sophiachan0430@gmail.com>
2025-07-21 13:47:09 +0300

    prepare for the render

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const app = express()
 // get the port from env variable based on environment
 const PORT = process.env.NODE_ENV === 'test'
   ? 8080
-  : (process.env.PORT || 5000)  // 生产环境使用 PORT 环境变量或 5000
-
+  : (process.env.PORT || 5000)
 app.use(express.static('dist'))
-
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
